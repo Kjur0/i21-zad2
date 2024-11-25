dc839e240ceb7a45d32b1a8217e39b0da3a3f469
uczen <uczen@S121K16>
2024-11-25 14:16:03 +0100

    Dodaj pliki projektów.

---
+++ AnimalRecord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace i21_zad2
+{
+    public class AnimalRecord
+    {
+        public string Gatunek { get; set; }
+        public string RodzajSzkoły { get; set; }
+        public string Szkoła { get; set; }
+        public string Klasa { get; set; }
+        public int IlośćMiłośników { get; set; }
+
+        public AnimalRecord() { }
+
+        public AnimalRecord(string gatunek, string rodzajSzkoły, string szkoła, string klasa, int ilośćMiłośników)
+        {
+            Gatunek = gatunek;
+            RodzajSzkoły = rodzajSzkoły;
+            Szkoła = szkoła;
+            Klasa = klasa;
+            IlośćMiłośników = ilośćMiłośników;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace i21_zad2
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<AnimalRecord> AnimalRecords { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            AnimalRecords = new ObservableCollection<AnimalRecord>();
+            AnimalDataGrid.ItemsSource = AnimalRecords;
+
+            AnimalRecords.Add(new AnimalRecord("Pies", "Technikum", "TM1", "3TC", 100));
+            AnimalRecords.Add(new AnimalRecord("Kot", "Liceum", "LXXX LO", "3LA", 50));
+        }
+
+        private void DataGrid_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+            {
+                var selectedItem = AnimalDataGrid.SelectedItem as AnimalRecord;
+                if (selectedItem != null) AnimalRecords.Remove(selectedItem);
+            }
+        }
+
+        private void AddNewRecord(object sender, RoutedEventArgs e)
+        {
+            AnimalRecords.Add(new AnimalRecord());
+        }
+    }
+}
